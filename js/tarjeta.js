0f12f301f0e9c36111b0177149e0fce78b3c9662
Adrian Fernandez <viadfeur@gmail.com>
2024-08-12 22:20:18 -0500

    Modificacion en responsividad del proyecto y transicion en el modal

--- js/tarjeta.js
+++ js/tarjeta.js
@@ -12,42 +12,16 @@ export default function tarjeta(pagInicio){
         let imgTarjeta = document.createElement('img');
         imgTarjeta.src = targeta.rutaImg;
         imgTarjeta.alt = targeta.alt;
-        imgTarjeta.className = targeta.clase[2];
+        imgTarjeta.className = targeta.clase[2];        
 
-        let divContenidoTarjeta = document.createElement('div');
-
-        divContenidoTarjeta.classList.add(targeta.clase[3])
-
-        let tituloTarjeta = document.createElement('h2');
-        tituloTarjeta.className = targeta.clase[4]
-        tituloTarjeta.textContent = targeta.titulo;
-
-        let parrafoTarjeta = document.createElement('p');
-        parrafoTarjeta.className = targeta.clase[5]
-        parrafoTarjeta.textContent = targeta.descripcion.slice(0, 50) + '...';
-
-        let divTarjetaContacto = document.createElement('div');
-        divTarjetaContacto.className = targeta.clase[6];
-
-        let parrafoContacto = document.createElement('p');
-        parrafoContacto.className = targeta.clase[7];
-        parrafoContacto.textContent = targeta.contacto;
-
-        divTarjetaContacto.appendChild(parrafoContacto);
-
-        divContenidoTarjeta.appendChild(tituloTarjeta);
-        divContenidoTarjeta.appendChild(parrafoTarjeta);
-        divContenidoTarjeta.appendChild(divTarjetaContacto);
-
-        divTarjeta.appendChild(imgTarjeta);
-        divTarjeta.appendChild(divContenidoTarjeta);
+        divTarjeta.appendChild(imgTarjeta);        
 
         divTarjeta.addEventListener('click', () => {
             modalBody.innerHTML = `
               <img src="${targeta.rutaImg}" alt="${targeta.alt}" class="${targeta.clase[2]}">
-              <h2>${targeta.titulo}</h2>
-              <p>${targeta.descripcion}</p>
-              <p>${targeta.contacto}</p>
+              <h2 class= "${targeta.clase[4]}">${targeta.titulo}</h2>
+              <p class="${targeta.clase[5]}">${targeta.descripcion}</p>
+              <p class="${targeta.clase[7]}">${targeta.contacto}</p>
             `;
             modal.classList.add('show');
             document.querySelector('.modal-content').classList.add('show');
--- js/utilidades.js
+++ js/utilidades.js
@@ -1,3 +1,6 @@
+const pathInicial = '/acabados-fernandez'
+const pathImages = '/imagenes/imagenes-targetas/'
+
 export const listaEnlaces = [
     { href: "#", dataView:"inicio", text: "Inicio" },
     { href: "#", dataView:"servicios", text: "Servicios" },
@@ -5,7 +8,7 @@ export const listaEnlaces = [
 ]
 
 export const imagenFondo =[{
-    ruta: '/acabados-fernandez/imagenes/imagenes-targetas/tarjeta-acabados-fernandez.webp',
+    ruta: '/imagenes/imagenes-targetas/tarjeta-acabados-fernandez.webp',
     alt: 'Imagen tarjeta presentacion',
     clase: 'imagen-fondo'
 }]
@@ -34,7 +37,7 @@ export const targetas = [
                 'tarjeta-descripcion',
                 'tarjeta-contacto',
                 'tarjeta-telefono'], 
-        rutaImg :'/acabados-fernandez/imagenes/imagenes-targetas/pasillo.webp', 
+        rutaImg :`${pathImages}pasillo.webp`, 
         alt: 'Estucos y pinturas', 
         titulo:'Estucos y pinturas', 
         descripcion: 'Ofrecemos aplicaciones profesionales de estucos y pinturas para interiores y exteriores, garantizando acabados duraderos y estéticos con técnicas avanzadas y materiales de primera calidad. Transformamos cualquier espacio con estucos texturizados y una amplia gama de colores y acabados de pintura.',
@@ -49,7 +52,7 @@ export const targetas = [
                 'tarjeta-descripcion',
                 'tarjeta-contacto',
                 'tarjeta-telefono'], 
-        rutaImg :'/acabados-fernandez/imagenes/imagenes-targetas/casa-patricia.webp', 
+        rutaImg :`${pathImages}casa-patricia.webp`, 
         alt: 'sala apartamento', 
         titulo:'Obra Civil en General', 
         descripcion: 'Proveemos servicios integrales de planificación, construcción, remodelación y mantenimiento de estructuras, garantizando alta calidad y cumplimiento de normativas para proyectos duraderos y eficientes.',
@@ -64,7 +67,7 @@ export const targetas = [
                 'tarjeta-descripcion',
                 'tarjeta-contacto',
                 'tarjeta-telefono'], 
-        rutaImg :'/acabados-fernandez/imagenes/imagenes-targetas/cocina-drywall.webp', 
+        rutaImg :`${pathImages}cocina-drywall.webp`, 
         alt: 'lamparas', 
         titulo:'Instalación Drywall', 
         descripcion: 'Este servicio se especializa en la creación y ejecución de diseños innovadores y funcionales en cielorrasos utilizando paneles de drywall. Ofrecemos soluciones personalizadas para mejorar la estética y la funcionalidad de espacios interiores, integrando características como iluminación empotrada, formas geométricas y acabados decorativos. Ideal para proyectos residenciales y comerciales que buscan un toque moderno y elegante en sus techos',
@@ -79,7 +82,7 @@ export const targetas = [
                 'tarjeta-descripcion',
                 'tarjeta-contacto',
                 'tarjeta-telefono'], 
-        rutaImg :'/acabados-fernandez/imagenes/imagenes-targetas/cocina-viviana.webp', 
+        rutaImg :`${pathImages}cocina-viviana.webp`, 
         alt: 'cocina', 
         titulo:'Remodelacion en Areas parciales', 
         descripcion: 'Ofrecemos servicios profesionales de instalación de enchapes en pisos, paredes y otras superficies, utilizando materiales de alta calidad y técnicas precisas para acabados duraderos y estéticos. Nos especializamos en cerámica, porcelanato, piedra natural y más, adaptándonos a las necesidades y preferencias de cada cliente.',
@@ -94,7 +97,7 @@ export const targetas = [
                 'tarjeta-descripcion',
                 'tarjeta-contacto',
                 'tarjeta-telefono'], 
-        rutaImg :'/acabados-fernandez/imagenes/imagenes-targetas/cocina-claudia.webp', 
+        rutaImg :`${pathImages}cocina-claudia.webp`, 
         alt: 'cocina', 
         titulo:'Instalacion de Enchapes', 
         descripcion: 'Ofrecemos instalación, reparación y mantenimiento de sistemas de agua y desagüe, manejando tuberías, grifos y soluciones para fugas y obstrucciones con eficacia y durabilidad.',
@@ -109,7 +112,7 @@ export const targetas = [
                 'tarjeta-descripcion',
                 'tarjeta-contacto',
                 'tarjeta-telefono'], 
-        rutaImg :'/acabados-fernandez/imagenes/imagenes-targetas/baño-madrid.webp', 
+        rutaImg :`${pathImages}baño-madrid.webp`, 
         alt: 'baño', 
         titulo:'Remodelación de Baños', 
         descripcion: 'Proporcionamos servicios integrales de acabados para interiores y exteriores, incluyendo trabajos de pintura, estucos, enchapes y más. Nos especializamos en lograr resultados estéticos y funcionales que transforman y embellecen cualquier espacio, utilizando materiales de alta calidad y técnicas profesionales.',
@@ -124,7 +127,7 @@ export const targetas = [
                 'tarjeta-descripcion',
                 'tarjeta-contacto',
                 'tarjeta-telefono'], 
-        rutaImg :'/acabados-fernandez/imagenes/imagenes-targetas/nichos-madrid.webp', 
+        rutaImg :`${pathImages}nichos-madrid.webp`, 
         alt: 'nichos', 
         titulo:'Acabados en General', 
         descripcion: 'Proporcionamos servicios integrales de acabados para interiores y exteriores, incluyendo trabajos de pintura, estucos, enchapes y más. Nos especializamos en lograr resultados estéticos y funcionales que transforman y embellecen cualquier espacio, utilizando materiales de alta calidad y técnicas profesionales.',
@@ -139,7 +142,7 @@ export const targetas = [
                 'tarjeta-descripcion',
                 'tarjeta-contacto',
                 'tarjeta-telefono'], 
-        rutaImg :'/acabados-fernandez/imagenes/imagenes-targetas/muro-fachaleta-madrid.webp', 
+        rutaImg :`${pathImages}muro-fachaleta-madrid.webp`, 
         alt: 'lamparas', 
         titulo:'Acabados en General', 
         descripcion: 'Proporcionamos servicios integrales de acabados para interiores y exteriores, incluyendo trabajos de pintura, estucos, enchapes y más. Nos especializamos en lograr resultados estéticos y funcionales que transforman y embellecen cualquier espacio, utilizando materiales de alta calidad y técnicas profesionales.',
@@ -154,7 +157,7 @@ export const targetas = [
                 'tarjeta-descripcion',
                 'tarjeta-contacto',
                 'tarjeta-telefono'], 
-        rutaImg :'/acabados-fernandez/imagenes/imagenes-targetas/apartamentoMadrid.webp', 
+        rutaImg :`${pathImages}apartamentoMadrid.webp`, 
         alt: 'lamparas', 
         titulo:'Acabados en General', 
         descripcion: 'Proporcionamos servicios integrales de acabados para interiores y exteriores, incluyendo trabajos de pintura, estucos, enchapes y más. Nos especializamos en lograr resultados estéticos y funcionales que transforman y embellecen cualquier espacio, utilizando materiales de alta calidad y técnicas profesionales.',
